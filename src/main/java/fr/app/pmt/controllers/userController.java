b1463fef9c272d40da133439b04dbecdfa68878f
akramescheba <meschebajordy@gmail.com>
2025-03-02 16:31:43 +0100

    Configuration des acces mysql

--- src/main/java/fr/app/pmt/controllers/userController.java
+++ src/main/java/fr/app/pmt/controllers/userController.java
@@ -2,7 +2,6 @@ package fr.app.pmt.controllers;
 
 import java.util.List;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +21,7 @@ import fr.app.pmt.services.UserService;
 
 @RestController
 //@RequestMapping("/utilisateur")
-@CrossOrigin(origins="http://localhost:4200/")
+@CrossOrigin(origins="https://angular-frontend.up.railway.app/", allowedHeaders = "*")
 
 public class userController {
 	
@@ -41,6 +39,7 @@ public class userController {
 		Users users = userService.findById(id);
 		
 		if(users==null) {
+			
 			
 		}return users;
 	}

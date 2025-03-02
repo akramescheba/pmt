f1cf814e18cffd5f13114733e92e98ed3b7cd82a
akramescheba <meschebajordy@gmail.com>
2025-03-02 15:35:57 +0100

    Configuration du cors

--- backend/src/main/java/fr/app/pmt/controllers/userController.java
+++ backend/src/main/java/fr/app/pmt/controllers/userController.java
@@ -2,7 +2,6 @@ package fr.app.pmt.controllers;
 
 import java.util.List;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +21,7 @@ import fr.app.pmt.services.UserService;
 
 @RestController
 //@RequestMapping("/utilisateur")
-@CrossOrigin(origins="https://strong-dedication-production.up.railway.app/")
+@CrossOrigin(origins="https://strong-dedication-production.up.railway.app")
 
 public class userController {
 	

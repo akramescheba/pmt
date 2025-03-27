c3c71da8bc37a5f26932be2009a864614a42dae7
akramescheba <meschebajordy@gmail.com>
2025-03-28 00:52:32 +0100

    configuration de JEST pour les tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,35 +1,13 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { CommonModule } from '@angular/common';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ToastrModule } from 'ngx-toastr';
 
+describe('AppComponent', ()=>{
+  let fixture: AppComponent;
+  beforeEach( () => {
+    fixture = new AppComponent();
+  })
 
-describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let app:AppComponent ;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent,BrowserAnimationsModule,  RouterTestingModule, CommonModule,  HttpClientTestingModule, ToastrModule.forRoot()],
-    }).compileComponents();
-    fixture = TestBed.createComponent(AppComponent);
-    app = fixture.componentInstance;
+  it('should have title', ()=>{
+    expect(fixture.title).toEqual('app');
   });
 
-  
-  it('should create the app', () => {
-  
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'app' title`, () => {
-  
-    expect(app.title).toEqual('app');
-  });
-
-
-
-
-});
+})
